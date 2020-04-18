cad83c36a47abb6e0233850fe72832a9245cd62d
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-18 15:57:10 -0700

    fix dependencies

--- src/main.js
+++ src/main.js
@@ -2,17 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vSelect from 'vue-select';
-
 import VeeValidate from 'vee-validate';
-// import Vuetify from 'vuetify';
 import vueHeadful from 'vue-headful';
-
-import Autocomplete from '@trevoreyre/autocomplete-vue';
-import '@trevoreyre/autocomplete-vue/dist/style.css';
 import vuetify from './plugins/vuetify';
-
-Vue.use(Autocomplete)
-
 
 Vue.use(VeeValidate);
 Vue.component('vue-headful', vueHeadful);
