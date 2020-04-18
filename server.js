e816dcbc85cd075a16a83a26bdd4e39c457ba343
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-18 15:51:37 -0700

    fix dependencies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const express = require('express')
+const path = require('path')
 
 
 const app = express()
@@ -12,7 +13,12 @@ app.use(bodyParser.json())
 app.use(cors())
 
 //Serve the Vue server
-app.use(serveStatic(__dirname + "/dist"));
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port);
