376561a1b5500e39961257c48a69b42014696892
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-19 16:47:22 -0700

    configure ssl

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ app.use(bodyParser.json())
 app.use(cors())
 
 //Serve the Vue server
-// app.use(serveStatic(path.join(__dirname, '/dist')))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-    res.send('iWlXXNyH5BvzPDME6HnC8ecC1n_yKllLRHfxvch1Dn8.Wb8CZdX0yd3r5sR72JXP91gNsPhngWZLi8jvoNQN4no')
-})
+
 
 
 const port = process.env.PORT || 5000;
