0858bbf55c4761d6efde807445a36e2647b9d4d5
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-19 15:20:37 -0700

    fix dependencies

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(cors())
 //Serve the Vue server
 app.use(serveStatic(path.join(__dirname, '/dist')))
 
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+    res.send('Yljp4RvcyzSl8V2aGPs6lEjWv0G6c9AIVuO4X1HKymo.Wb8CZdX0yd3r5sR72JXP91gNsPhngWZLi8jvoNQN4no')
+})
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
