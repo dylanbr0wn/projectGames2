4061c0cc6c53b6fa207d3698dc318b0ed7604ccd
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-19 17:12:01 -0700

    fix api link

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     var instance = axios.create({
-        baseURL: (process.env.API_URL) ? process.env.API_URL : 'http://localhost:3000/',
+        baseURL: process.env.VUE_APP_API_URL,
 
     });
     return instance
