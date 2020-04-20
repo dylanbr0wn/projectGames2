89b1ebde62ffa16763d1288897102f34d0b9331f
dylanbr0wn <dylan.bro97@gmail.com>
2020-04-19 19:53:51 -0700

    compression

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // server.js
-const serveStatic = require('serve-static');
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const express = require('express')
-const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
 const history = require('connect-history-api-fallback');
+const compression = require('compression');
 
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(compression());
 
 app.use(function(req, res,next){
     if(req.headers["x-forwarded-proto"] === "https"){
@@ -19,15 +19,9 @@ app.use(function(req, res,next){
     }
     res.redirect("https://" + req.hostname + req.url);
 });
-app.use(history())
-
-
-
+app.use(history());
 //Serve the Vue server
-app.use(express.static('dist'))
-
-
-
+app.use(express.static('dist'));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
